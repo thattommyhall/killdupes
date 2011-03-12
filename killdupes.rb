ea1de4fb6231d0cc8c1b84866d501d085b8c8b63
Tom Hall <thattommyhall@gmail.com>
2011-03-12 23:29:29 +0000

    working, need to sort init_hash out

--- killdupes.rb
+++ killdupes.rb
@@ -8,7 +8,10 @@ $BUFLEN = 1024
 def init_hash(file)
   hash = Digest::MD5.new
   open(file, "r") do |io|
-    hash.update(io.readpartial($BUFLEN))
+    while (!io.eof)
+      readBuf = io.readpartial($BUFLEN)
+      hash.update(readBuf)
+    end
   end
   return hash.hexdigest
 end
@@ -24,12 +27,6 @@ def full_hash(file)
   return hash.hexdigest
 end
 
-ARGV.each do |file|
-  puts "filename: #{file}"
-  puts "full_hash: #{full_hash(file)}"
-  puts "init_hash: #{init_hash(file)}"
-end
-
 class Hash
   def delete_ones
     delete_if {|key,val| val.size == 1}
@@ -39,10 +36,29 @@ end
 files_by_size = {}
 files_by_size.default = []
 
-Find.find('./test') do |path|
+Find.find('/home/tom/Dropbox/') do |path|
   if File.file? path
     files_by_size[File.size(path)] += [path]
   end
 end
 
-puts files_by_size.delete_ones
+files_by_init_hash = {}
+files_by_init_hash.default = []
+
+files_by_size.delete_ones.values.flatten.each do |path|
+  files_by_init_hash[init_hash(path)] += [path]
+end
+
+files_by_full_hash = {}
+files_by_full_hash.default = []
+
+files_by_init_hash.delete_ones.values.flatten.each do |path|
+  files_by_full_hash[full_hash(path)] += [path]
+end
+
+files_by_full_hash.values.each do |list_of_matches|
+  puts '******************************'
+  list_of_matches.each {|match| puts match}
+end
+
+
